88c14e554d83918063638a8c9a1d7976283474d6
soumalya1902 <149316877+soumalya1902@users.noreply.github.com>
2024-04-11 23:17:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ with st.sidebar:
         'Blood Cancer Prediction',
         'Prostate Cancer Prediction',       
     ],
-        icons=['house-add','activity', 'heart-pulse', 'person-add','bar-chart','person-plus','lungs','heart','gender-female','gear', 'book','droplet','droplet','virus'],
+        icons=['house-add','activity', 'heart-pulse', 'person-add','bar-chart','person-plus','lungs','heart','gender-female','gear', 'book','droplet','droplet','clipboard2-pulse'],
         default_index=0)
     
 # About App
